9df9c26f753e889508c3c3fcdd4f8d8376dc6e74
NeelSavsani <neelsavsani7@gmail.com>
2026-01-17 11:52:30 +0530

    Changing case in css in login.php

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <title>Login | EXPIROCHAIN</title>
     <link rel="shortcut icon" href="images/favicon/android-chrome-192x192.png" />
-    <link rel="stylesheet" href="CSS/register.css" />
+    <link rel="stylesheet" href="css/register.css" />
   </head>
 
   <body>
